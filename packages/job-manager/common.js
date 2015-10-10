9972899a5c47d63672878b7d24b27547bbad6d4a
Max Bruchmann <bruchmann@signal7.de>
2015-10-10 22:05:42 +0200

    fixed job name

--- packages/job-manager/common.js
+++ packages/job-manager/common.js
@@ -6,6 +6,10 @@ FS.JobManager = {
   _registeredJobTypes: [],
   _registeredJobWorkers: []
 };
-var jobCollectionName = 'cfs_jobManager' + (process.env.NODE_ID || '_local_');
+var jobCollectionName = 'cfs_jobManager' ;
+
+if(Meteor.isServer){
+  jobCollectionName +=+ (process.env.NODE_ID || '_local_');
+}
 // TODO: Allow custom options
 FS.JobManager.jobCollection =  new JobCollection(jobCollectionName);
